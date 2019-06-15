18c2ca4b129f64871cb10afd694e98a522314ebd
vaishali-ag <vaishali02071997@gmail.com>
2019-06-15 23:39:21 +0530

    fragment profie added

--- app/src/main/java/vaisahli/agrawal/va/MainActivity.java
+++ app/src/main/java/vaisahli/agrawal/va/MainActivity.java
@@ -1,13 +1,20 @@
 package vaisahli.agrawal.va;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewManager;
 
 public class MainActivity extends AppCompatActivity {
+TabLayout tabLayout;
+ViewManager viewManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout=findViewById(R.id.tablayout);
+        viewManager=findViewById(R.id.viewmanager);
+
     }
 }
